627433d0d5634ce7694ba7781b06403e4ffd7ca2
Daniel Liebler <DLiebler13@gmail.com>
2023-12-16 14:57:38 +0100

    solve 16-1

--- aoc_15/src/main.rs
+++ aoc_15/src/main.rs
@@ -6,16 +6,55 @@ use pest::iterators::Pairs;
 use pest::Parser;
 use pest_derive::Parser;
 
+use std::collections::HashMap;
+
 #[derive(Parser)]
 #[grammar = "grammar.pest"]
 struct MyParser;
 
-fn new_entry(current_value: &u128, entry: char) -> u128 {
-    return ((*current_value + (entry as u128)) * 17) % 256;
+struct Lens {
+    literal: String,
+    hashed: usize,
+    focal_len: usize,
 }
 
-fn analyze_entry(parsed: Pair<'_, Rule>) -> u128 {
-    let mut current_value: u128 = 0;
+fn hash_step(current_value: &usize, entry: char) -> usize {
+    return ((*current_value + (entry as usize)) * 17) % 256;
+}
+
+fn add_entry(hashmap: &mut HashMap<usize, Vec<Lens>>, lens: Lens) {
+    match hashmap.get_mut(&lens.hashed) {
+        Some(entry) => match entry.iter_mut().find(|l| l.literal == lens.literal) {
+            Some(l) => l.focal_len = lens.focal_len,
+            None => entry.push(lens),
+        },
+        None => {
+            hashmap.insert(lens.hashed, vec![lens]);
+        }
+    }
+}
+
+fn remove_entry(hashmap: &mut HashMap<usize, Vec<Lens>>, hashed: usize, literal: String) {
+    match hashmap.get_mut(&hashed) {
+        Some(entry) => match entry.iter().position(|l| l.literal == literal) {
+            Some(p) => {
+                entry.remove(p);
+            }
+            None => {
+                println!(
+                    "could not remove nonexistent lens, '{}'({})",
+                    literal, hashed
+                );
+            }
+        },
+        None => {
+            println!("trying to remove, but box does not exist")
+        }
+    }
+}
+
+fn analyze_hash(parsed: Pair<'_, Rule>) -> usize {
+    let mut current_value: usize = 0;
     for entry in parsed.into_inner() {
         match entry.as_rule() {
             Rule::character => {
@@ -23,7 +62,7 @@ fn analyze_entry(parsed: Pair<'_, Rule>) -> u128 {
                 match entry_char {
                     Some(',') => println!("Error: unexpected ,"),
                     Some('\n') => println!("Error: unexpected NEWLINE"),
-                    Some(c) => current_value = new_entry(&current_value, c),
+                    Some(c) => current_value = hash_step(&current_value, c),
                     None => println!("Error: unexpected nothing"),
                 }
             }
@@ -31,23 +70,96 @@ fn analyze_entry(parsed: Pair<'_, Rule>) -> u128 {
                 println!("EOI {}", entry.as_str());
             }
             _ => {
-                println!("UNEXPECTED PARSE(report) {}", entry.as_str());
+                println!("UNEXPECTED PARSE(hash) {}", entry.as_str());
             }
         }
     }
     return current_value;
 }
+fn analyze_add_entry(parsed: Pair<'_, Rule>, hashmap: &mut HashMap<usize, Vec<Lens>>) {
+    let mut literal: Option<String> = None;
+    let mut hashed: Option<usize> = None;
+    let mut focal_len: Option<usize> = None;
+    for entry in parsed.into_inner() {
+        match entry.as_rule() {
+            Rule::hash => {
+                literal = Some(entry.as_str().to_string());
+                hashed = Some(analyze_hash(entry));
+            }
+            Rule::number => {
+                focal_len = Some(
+                    entry
+                        .as_str()
+                        .to_string()
+                        .parse()
+                        .expect("could not parse number"),
+                )
+            }
+            Rule::EOI => {
+                println!("EOI {}", entry.as_str());
+            }
+            _ => {
+                println!("UNEXPECTED PARSE(add) {}", entry.as_str());
+            }
+        }
+    }
+    match (literal, hashed, focal_len) {
+        (Some(literal), Some(hashed), Some(focal_len)) => add_entry(
+            hashmap,
+            Lens {
+                literal: literal,
+                hashed: hashed,
+                focal_len: focal_len,
+            },
+        ),
+        (_, _, _) => println!("Err, not all arguments for add supplied."),
+    }
+}
+fn analyze_remove_entry(parsed: Pair<'_, Rule>, hashmap: &mut HashMap<usize, Vec<Lens>>) {
+    let mut literal: Option<String> = None;
+    let mut hashed: Option<usize> = None;
+    for entry in parsed.into_inner() {
+        match entry.as_rule() {
+            Rule::hash => {
+                literal = Some(entry.as_str().to_string());
+                hashed = Some(analyze_hash(entry));
+            }
+            Rule::EOI => {
+                println!("EOI {}", entry.as_str());
+            }
+            _ => {
+                println!("UNEXPECTED PARSE(remove) {}", entry.as_str());
+            }
+        }
+    }
+    match (literal, hashed) {
+        (Some(literal), Some(hashed)) => remove_entry(hashmap, hashed, literal),
+        (_, _) => println!("Err, not all arguments for add supplied."),
+    }
+}
 
-fn analyze_file(parsed: &mut Pairs<'_, Rule>) -> Vec<u128> {
-    let mut list: Vec<u128> = Vec::new();
+fn analyze_entry(parsed: Pair<'_, Rule>, hashmap: &mut HashMap<usize, Vec<Lens>>) {
+    for entry in parsed.into_inner() {
+        match entry.as_rule() {
+            Rule::add_entry => analyze_add_entry(entry, hashmap),
+            Rule::remove_entry => analyze_remove_entry(entry, hashmap),
+            Rule::EOI => {
+                println!("EOI {}", entry.as_str());
+            }
+            _ => {
+                println!("UNEXPECTED PARSE(entry) {}", entry.as_str());
+            }
+        }
+    }
+}
+
+fn analyze_file(parsed: &mut Pairs<'_, Rule>) -> HashMap<usize, Vec<Lens>> {
+    let mut hashmap: HashMap<usize, Vec<Lens>> = HashMap::new();
     let unwrapped = parsed.next().unwrap();
     for entry in unwrapped.into_inner() {
         match entry.as_rule() {
             Rule::entry => {
-                let str = entry.as_str();
-                let value = analyze_entry(entry);
-                list.push(value);
-                println!("Value is {} for '{}'", value, str);
+                analyze_entry(entry, &mut hashmap);
             }
             Rule::EOI => {
                 println!("EOI {}", entry.as_str());
@@ -57,7 +169,24 @@ fn analyze_file(parsed: &mut Pairs<'_, Rule>) -> Vec<u128> {
             }
         }
     }
-    return list;
+    return hashmap;
+}
+fn get_focal_power(lens: &Lens, slot: usize) -> usize {
+    return (lens.hashed + 1) * (slot + 1) * lens.focal_len;
+}
+fn sum_focal_power(hashmap: &HashMap<usize, Vec<Lens>>) -> usize {
+    let mut sum = 0;
+    for (_box_id, lenses) in hashmap.iter() {
+        for (slot, lens) in lenses.iter().enumerate() {
+            let focal_power = get_focal_power(lens, slot);
+            sum += focal_power;
+            println!(
+                "Focal power of '{}'({}) @{} pow{} is {}",
+                lens.literal, lens.hashed, slot, lens.focal_len, focal_power
+            );
+        }
+    }
+    return sum;
 }
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -70,7 +199,8 @@ fn main() {
     match parse_result {
         Ok(mut result) => {
             let list = analyze_file(&mut result);
-            println!("Sum is {}", list.iter().fold(0, |accu, i| accu + i));
+            let focal_power = sum_focal_power(&list);
+            println!("focal power is {}", focal_power);
         }
         Err(result) => {
             println!("ERR:  Could not parse file: {result}");
--- aoc_16/src/main.rs
+++ aoc_16/src/main.rs
@@ -1,3 +1,357 @@
+use std::env;
+use std::fs;
+
+use pest::iterators::Pair;
+use pest::iterators::Pairs;
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+struct MyParser;
+
+enum TileContent {
+    Empty,
+    SplitterHorizontal,
+    SplitterVertical,
+    MirrorTopLeft,
+    MirrorTopRight,
+}
+struct Tile {
+    content: TileContent,
+    power: usize,
+    walked_up: bool,
+    walked_left: bool,
+    walked_right: bool,
+    walked_down: bool,
+}
+
+#[derive(PartialEq, Clone, Eq)]
+enum Direction {
+    Up,
+    Left,
+    Down,
+    Right,
+}
+#[derive(PartialEq, Clone, Eq)]
+struct Ray {
+    x: usize,
+    y: usize,
+    direction: Direction,
+}
+#[derive(PartialEq, Clone, Eq)]
+struct Dimension {
+    x: usize,
+    y: usize,
+}
+
+fn analyze_tile(parsed: Pair<'_, Rule>) -> Result<Tile, &'static str> {
+    let mut tile: Option<Tile> = None;
+    for entry in parsed.into_inner() {
+        match entry.as_rule() {
+            Rule::empty => {
+                tile = Some(Tile {
+                    content: TileContent::Empty,
+                    power: 0,
+                    walked_up: false,
+                    walked_left: false,
+                    walked_right: false,
+                    walked_down: false,
+                })
+            }
+            Rule::splitter_h => {
+                tile = Some(Tile {
+                    content: TileContent::SplitterHorizontal,
+                    power: 0,
+                    walked_up: false,
+                    walked_left: false,
+                    walked_right: false,
+                    walked_down: false,
+                })
+            }
+            Rule::splitter_v => {
+                tile = Some(Tile {
+                    content: TileContent::SplitterVertical,
+                    power: 0,
+                    walked_up: false,
+                    walked_left: false,
+                    walked_right: false,
+                    walked_down: false,
+                })
+            }
+            Rule::mirror_tl => {
+                tile = Some(Tile {
+                    content: TileContent::MirrorTopLeft,
+                    power: 0,
+                    walked_up: false,
+                    walked_left: false,
+                    walked_right: false,
+                    walked_down: false,
+                })
+            }
+            Rule::mirror_tr => {
+                tile = Some(Tile {
+                    content: TileContent::MirrorTopRight,
+                    power: 0,
+                    walked_up: false,
+                    walked_left: false,
+                    walked_right: false,
+                    walked_down: false,
+                })
+            }
+            Rule::EOI => {
+                println!("EOI {}", entry.as_str());
+            }
+            _ => {
+                println!("UNEXPECTED PARSE(entry) {}", entry.as_str());
+            }
+        }
+    }
+    match tile {
+        Some(tile) => Ok(tile),
+        None => Err("Did not find Tile specification"),
+    }
+}
+fn analyze_line(parsed: Pair<'_, Rule>) -> Vec<Tile> {
+    let mut line: Vec<Tile> = Vec::new();
+    for entry in parsed.into_inner() {
+        match entry.as_rule() {
+            Rule::tile => match analyze_tile(entry) {
+                Ok(tile) => line.push(tile),
+                Err(error) => println!("Error parsing tile: '{error}'"),
+            },
+            Rule::EOI => {
+                println!("EOI {}", entry.as_str());
+            }
+            _ => {
+                println!("UNEXPECTED PARSE(entry) {}", entry.as_str());
+            }
+        }
+    }
+    return line;
+}
+
+fn analyze_file(parsed: &mut Pairs<'_, Rule>) -> Vec<Vec<Tile>> {
+    let mut grid: Vec<Vec<Tile>> = Vec::new();
+    let unwrapped = parsed.next().unwrap();
+    for entry in unwrapped.into_inner() {
+        match entry.as_rule() {
+            Rule::line => {
+                grid.push(analyze_line(entry));
+            }
+            Rule::EOI => {
+                println!("EOI {}", entry.as_str());
+            }
+            _ => {
+                println!("UNEXPECTED PARSE(File) {}", entry.as_str());
+            }
+        }
+    }
+    return grid;
+}
+
+fn get_tile(grid: &mut Vec<Vec<Tile>>, x: usize, y: usize) -> &mut Tile {
+    return grid
+        .get_mut(y)
+        .and_then(|f| f.get_mut(x))
+        .expect("Could not find Tile");
+}
+fn get_next_coords(ray: &Ray, dimensions: &Dimension) -> Option<(usize, usize)> {
+    match ray.direction {
+        Direction::Up => match ray.y {
+            0 => return None,
+            _ => return Some((ray.x, ray.y - 1)),
+        },
+        Direction::Left => match ray.x {
+            0 => return None,
+            _ => return Some((ray.x - 1, ray.y)),
+        },
+        Direction::Down => match ray.y {
+            y if y < dimensions.y - 1 => return Some((ray.x, ray.y + 1)),
+            _ => return None,
+        },
+        Direction::Right => match ray.x {
+            x if x < dimensions.x - 1 => return Some((ray.x + 1, ray.y)),
+            _ => return None,
+        },
+    }
+}
+fn move_ray(
+    ray: &mut Ray,
+    dimensions: &Dimension,
+    remove_ray: &mut bool,
+    grid: &mut Vec<Vec<Tile>>,
+) {
+    let direction = ray.direction.clone();
+    match get_next_coords(ray, dimensions) {
+        Some((x, y)) => {
+            let tile = get_tile(grid, x, y);
+            let walked_marker = match direction {
+                Direction::Up => &mut tile.walked_up,
+                Direction::Left => &mut tile.walked_left,
+                Direction::Down => &mut tile.walked_down,
+                Direction::Right => &mut tile.walked_right,
+            };
+            if *walked_marker {
+                // already walked ray there
+                *remove_ray = true;
+            } else {
+                ray.x = x;
+                ray.y = y;
+                *walked_marker = true;
+            }
+        }
+        None => *remove_ray = true,
+    };
+}
+fn calculate_powers(grid: &mut Vec<Vec<Tile>>) {
+    let mut rays: Vec<Ray> = vec![Ray {
+        x: 0,
+        y: 0,
+        direction: Direction::Right,
+    }];
+
+    let dimensions = Dimension {
+        x: grid
+            .first()
+            .expect("could not access first line of grid")
+            .len(),
+        y: grid.len(),
+    };
+    loop {
+        let mut remove_ray = false;
+        let mut add_ray: Option<Ray> = None;
+        match rays.first_mut() {
+            Some(ray) => {
+                let tile = get_tile(grid, ray.x, ray.y);
+                tile.power += 1;
+                match tile.content {
+                    TileContent::Empty => move_ray(ray, &dimensions, &mut remove_ray, grid),
+                    TileContent::SplitterHorizontal => match ray.direction {
+                        Direction::Up | Direction::Down => {
+                            let mut ray2 = ray.clone();
+                            ray.direction = Direction::Left;
+                            ray2.direction = Direction::Right;
+                            let mut remove_ray_1 = false;
+                            let mut remove_ray_2 = false;
+
+                            move_ray(ray, &dimensions, &mut remove_ray_1, grid);
+                            move_ray(&mut ray2, &dimensions, &mut remove_ray_2, grid);
+
+                            match (remove_ray_1, remove_ray_2) {
+                                (true, true) => remove_ray = true,
+                                (true, false) => {
+                                    ray.direction = ray2.direction;
+                                    ray.x = ray2.x;
+                                    ray.y = ray2.y;
+                                }
+                                (false, true) => {}
+                                (false, false) => add_ray = Some(ray2),
+                            }
+                        }
+                        Direction::Left | Direction::Right => {
+                            move_ray(ray, &dimensions, &mut remove_ray, grid)
+                        }
+                    },
+                    TileContent::SplitterVertical => match ray.direction {
+                        Direction::Left | Direction::Right => {
+                            let mut ray2 = ray.clone();
+                            ray.direction = Direction::Up;
+                            ray2.direction = Direction::Down;
+                            let mut remove_ray_1 = false;
+                            let mut remove_ray_2 = false;
+
+                            move_ray(ray, &dimensions, &mut remove_ray_1, grid);
+                            move_ray(&mut ray2, &dimensions, &mut remove_ray_2, grid);
+
+                            match (remove_ray_1, remove_ray_2) {
+                                (true, true) => remove_ray = true,
+                                (true, false) => {
+                                    ray.direction = ray2.direction;
+                                    ray.x = ray2.x;
+                                    ray.y = ray2.y;
+                                }
+                                (false, true) => {}
+                                (false, false) => add_ray = Some(ray2),
+                            }
+                        }
+                        Direction::Up | Direction::Down => {
+                            move_ray(ray, &dimensions, &mut remove_ray, grid)
+                        }
+                    },
+                    TileContent::MirrorTopLeft => match ray.direction {
+                        Direction::Up => {
+                            ray.direction = Direction::Right;
+                            move_ray(ray, &dimensions, &mut remove_ray, grid);
+                        }
+                        Direction::Left => {
+                            ray.direction = Direction::Down;
+                            move_ray(ray, &dimensions, &mut remove_ray, grid);
+                        }
+                        Direction::Down => {
+                            ray.direction = Direction::Left;
+                            move_ray(ray, &dimensions, &mut remove_ray, grid);
+                        }
+                        Direction::Right => {
+                            ray.direction = Direction::Up;
+                            move_ray(ray, &dimensions, &mut remove_ray, grid);
+                        }
+                    },
+                    TileContent::MirrorTopRight => match ray.direction {
+                        Direction::Up => {
+                            ray.direction = Direction::Left;
+                            move_ray(ray, &dimensions, &mut remove_ray, grid);
+                        }
+                        Direction::Left => {
+                            ray.direction = Direction::Up;
+                            move_ray(ray, &dimensions, &mut remove_ray, grid);
+                        }
+                        Direction::Down => {
+                            ray.direction = Direction::Right;
+                            move_ray(ray, &dimensions, &mut remove_ray, grid);
+                        }
+                        Direction::Right => {
+                            ray.direction = Direction::Down;
+                            move_ray(ray, &dimensions, &mut remove_ray, grid);
+                        }
+                    },
+                }
+            }
+            None => break,
+        }
+        if remove_ray {
+            rays.remove(0);
+        }
+        match add_ray {
+            Some(ray) => rays.push(ray),
+            None => {}
+        }
+    }
+}
+
+fn sum_power(grid: &Vec<Vec<Tile>>) -> usize {
+    return grid
+        .iter()
+        .map(|line| line.iter().filter(|tile| tile.power >= 1).count())
+        .sum();
+}
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let path = &args[1];
+
+    let contents = fs::read_to_string(path).expect("Should have been able to read the file");
+
+    let parse_result = MyParser::parse(Rule::file, &contents);
+
+    match parse_result {
+        Ok(mut result) => {
+            let mut grid = analyze_file(&mut result);
+            calculate_powers(&mut grid);
+
+            println!("Power sum is {}", sum_power(&grid));
+        }
+        Err(result) => {
+            println!("ERR:  Could not parse file: {result}");
+        }
+    }
 }
